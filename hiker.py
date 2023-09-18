877e728751b61c17b35494efa0102624c4935d9b
Ayush_sccg <ayush1240945@gmail.com>
2023-09-18 12:13:47 +0545

    question

---
+++ hiker.py
@@ -0,0 +1,16 @@
+'''The starting files are unrelated to the exercise.
+
+They simply show syntax for writing and testing
+  o) a global function
+  o) an instance method
+Pick the style that best fits the exercise.
+Then delete the other one, along with this comment!
+'''
+
+def global_answer():
+    return 6 * 9
+
+class Hiker:
+
+    def instance_answer(self):
+        return global_answer()
